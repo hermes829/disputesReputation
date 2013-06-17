9c1b040ab6f31d4f8d8144d35d2510164652ca19
Shahryar Minhas <hermes829@gmail.com>
2013-06-17 01:14:49 -0400

    prelim results

--- RCode/Analysis/ECM.R
+++ RCode/Analysis/ECM.R
@@ -28,6 +28,47 @@ load('combinedData.rda')
 #     - Possible interaction between this and ICSID disputes
 
 # Variable manipulation
+vars <- c('ccode', 'cname', 'year', 'cyear',
+	'Investment.Profile', 'gdp.y', 'chgdp', 'polity', 
+	'signedbits', 'settle', 'conc_disputes', 'exprop')
+modelData <- combData[,vars]
+dim(modelData)
+
+# change from t-1
+countries <- unique(modelData$cname)
+years <- 1984:2011
+chData <- NULL
+for(ii in 1:length(countries)){
+	slice <- modelData[modelData$cname==countries[ii],]
+	for(jj in 2:length(years)){
+		cyear <- slice[slice$year==years[jj],'cyear']
+		tData <- slice[slice$year==years[jj],5:ncol(slice)]
+		t_1Data <- slice[slice$year==(years[jj]-1),5:ncol(slice)]
+		chSlice <- tData - t_1Data
+		colnames(chSlice) <- paste('ch_' ,colnames(tData),sep='')
+		chSlice <- cbind(cyear, chSlice)
+		chData <- rbind(chData, chSlice) }
+	print(ii) }
+dim(modelData); dim(chData)
+modelData <- merge(modelData, chData, by='cyear', all.x=T)
+dim(modelData); dim(chData)
+# dirty lag
+modelData <- ddply(modelData,.(ccode),transform,
+	Investment.ProfileLag = c(NA, Investment.Profile[-length(Investment.Profile)]))
+modelData <- ddply(modelData,.(ccode),transform,
+	gdp.yLag = c(NA, gdp.y[-length(gdp.y)]))
+modelData <- ddply(modelData,.(ccode),transform,
+	chgdpLag = c(NA, chgdp[-length(chgdp)]))
+modelData <- ddply(modelData,.(ccode),transform,
+	polityLag = c(NA, polity[-length(polity)]))
+modelData <- ddply(modelData,.(ccode),transform,
+	signedbitsLag = c(NA, signedbits[-length(signedbits)]))
+modelData <- ddply(modelData,.(ccode),transform,
+	settleLag = c(NA, settle[-length(settle)]))
+modelData <- ddply(modelData,.(ccode),transform,
+	conc_disputesLag = c(NA, conc_disputes[-length(conc_disputes)]))
+modelData <- ddply(modelData,.(ccode),transform,
+	expropLag = c(NA, exprop[-length(exprop)]))
 
 # polity correction
 combData[combData$polity==-88,'polity'] <- -10
@@ -36,8 +77,11 @@ combData[combData$polity==-66,'polity'] <- -10
 
 # conc_disputes spatial
 
+
 # Running model
+modelData <- na.omit(modelData)
 # Form of ECM
 # ∆Yt = α + β0*∆Xt - β1(Yt-1 - β2Xt-1) + ε
 formula = Investment.Profile ~ gdp.y + chgdp + polity + tradebalance + signedbits + 
 			settle + conc_disputes
+
