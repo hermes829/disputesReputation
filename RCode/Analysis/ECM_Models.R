95917ca530940a7d21b764482f00ef2f6874d819
Shahryar Minhas <hermes829@gmail.com>
2013-06-25 22:28:43 -0400

    created syntax to turn lmer results into apsrtable format

--- RCode/Analysis/ECM_Models.R
+++ RCode/Analysis/ECM_Models.R
@@ -43,39 +43,39 @@ means <- apply(modelSumm, 2, mean)
 sds <- apply(modelSumm, 2, sd)
 quants <- t(apply(modelSumm, 2, function(x) FUN=quantile(x,seq(0,1,.25))))
 summResults <- xtable(cbind(means, sds, quants),digits=2)
-setwd(paste(pathLatex,'/PrelimResults3',sep=''))
+setwd(paste(pathLatex,'/reModel_V1',sep=''))
 print(summResults, file='summResults.tex')
 
 # Form of ECM
 # ∆Yt = α + β0*∆Xt - β1(Yt-1 - β2Xt-1) + ε
-# dv = paste(vars[2], ' ~ ', vars[14], ' +') # Inv Profile
-dv = paste(vars[1], ' ~ ', vars[13], ' +') # Prop Rights
+dv = paste(vars[2], ' ~ ', vars[14], ' +') # Inv Profile
+# dv = paste(vars[1], ' ~ ', vars[13], ' +') # Prop Rights
 
 covsV1 = paste(vars[3], vars[4], vars[5],   
 	vars[15], vars[16], vars[17], 
-	# vars[6], vars[18], # rat bits
-	vars[7], vars[19], # sig bits
+	vars[6], vars[18], # rat bits
+	# vars[7], vars[19], # sig bits
 	vars[9], vars[21], # conc disp
 	paste('(1|',vars[25],')',sep=''), sep=' + ') 
 
 covsV1.5 = paste(vars[3], vars[4], vars[5], 
 	vars[15], vars[16], vars[17], 
-	# vars[6], vars[18], # rat bits
-	vars[7], vars[19], # sig bits
+	vars[6], vars[18], # rat bits
+	# vars[7], vars[19], # sig bits
 	vars[10], vars[22], # pend disp
 	paste('(1|',vars[25],')',sep=''), sep=' + ') 
 
 covsV2 = paste(vars[3], vars[4], vars[5], 
 	vars[15], vars[16], vars[17], 
-	# vars[6], vars[18], # rat bits 
-	vars[7], vars[19], # sig bits
+	vars[6], vars[18], # rat bits 
+	# vars[7], vars[19], # sig bits
 	vars[11], vars[23], # cp disp
 	paste('(1|',vars[25],')',sep=''), sep=' + ') 
 
 covsV3 = paste(vars[3], vars[4], vars[5], 
 	vars[15], vars[16], vars[17], 
-	# vars[6], vars[18], # rat bits 
-	vars[7], vars[19], # sig bits
+	vars[6], vars[18], # rat bits 
+	# vars[7], vars[19], # sig bits
 	vars[12], vars[24], # disp-settl
 	paste('(1|',vars[25],')',sep=''), sep=' + ') 
 
@@ -111,28 +111,50 @@ varsTable <- c(
 	'conc_disputesLag',
 	'pend_disputesLag',
 	'cp_disputesLag',
-	'disputesNoSettleLag',
+	'disputesNoSettleLag'
 	)
 
-# Old results using pooled models and apsrtable
-# model1<-lm(formulaV1,modelData);model1.5<-lm(formulaV1.5,modelData);
-# 	model2<-lm(formulaV2,modelData);model3<-lm(formulaV3,modelData)
 
-# modelLatex <- apsrtable(model1, model1.5, model2, model3, 
-# 	order='longest',
-# 	digits=3, caption.position='below', Sweave=T,
-# 	# caption='Here we show the results of a fixed effects regression on investment profile (DV=pch\\_Investment.Profile) using ratified BITs as a covariate, a number of dispute measures, and typical control variables.')
-# 	# caption='Here we show the results of a fixed effects regression on the protection of property rights (DV=pch\\_Property.Rights) using ratified BITs as a covariate, a number of dispute measures, and typical control variables.')
-# 	# caption='Here we show the results of a fixed effects regression on investment profile (DV=pch\\_Investment.Profile) using signed BITs as a covariate, a number of dispute measures, and typical control variables.')
-# 	caption='Here we show the results of a fixed effects regression on the protection of property rights (DV=pch\\_Property.Rights) using signed BITs as a covariate, a number of dispute measures, and typical control variables.')
+# Creating APSR like tables
+# tabEst <- paste(varsTable, 'Estimate', sep="__")
+# tabSE <- paste(varsTable, 'Std. Error', sep="__")
+tableResults <- matrix('', nrow=2*length(varsTable), ncol=5)
+digs = 3; noModels=4
+# rownames(tableResults) <- c(tabEst, tabSE)
+tableResults[,1] <- rep(varsTable,2)
+colnames(tableResults) <- c('Variable',paste('Model',1:noModels))
 
-# setwd(paste(pathLatex,'/PrelimResults3',sep=''))
+for(ii in 2:ncol(tableResults)){
+	temp <- modelSumm[[ii-1]]
+	temp <- temp[match(tableResults[,'Variable'], rownames(temp)),]
+	estims <- temp[1:length(varsTable),'Estimate']
+	estims <- round(as.numeric(as.character(estims)),digs)
+	tableResults[1:length(varsTable),ii] <- estims
+	serrors <- temp[(length(varsTable)+1):nrow(tableResults),'Std. Error']
+	serrors <- round(as.numeric(as.character(serrors)),digs)
+	serrors <- paste('(',serrors,')',sep='')
+	serrors <- ifelse(serrors=='(NA)','',serrors)
+	tableResults[(length(varsTable)+1):nrow(tableResults),ii] <- serrors
+}
 
-# # writeLines(modelLatex,file("modelResultsInvProfileV1.tex"))
-# # writeLines(modelLatex,file("modelResultsPropRightsV1.tex"))
+# Reorganizing rows and variable labels
+tableFinal <- NULL
+for(ii in 1:length(varsTable)){
+	temp <- cbind('', t(tableResults[ii+length(varsTable),2:ncol(tableResults)]))
+	tableFinal <- rbind(tableFinal, tableResults[ii,], temp) }
 
-# # writeLines(modelLatex,file("modelResultsInvProfileV2.tex"))
-# writeLines(modelLatex,file("modelResultsPropRightsV2.tex"))
+setwd(paste(pathLatex,'/reModel_V1',sep=''))
+print(xtable(tableFinal,
+	caption='Here we show the results of a random effects regression on investment profile (DV=pch\\_Investment.Profile) using ratified BITs as a covariate, a number of dispute measures, and typical control variables.'
+	# caption='Here we show the results of a random effects regression on the protection of property rights (DV=pch\\_Property.Rights) using ratified BITs as a covariate, a number of dispute measures, and typical control variables.'
+	# caption='Here we show the results of a random effects regression on investment profile (DV=pch\\_Investment.Profile) using signed BITs as a covariate, a number of dispute measures, and typical control variables.'
+	# caption='Here we show the results of a random effects regression on the protection of property rights (DV=pch\\_Property.Rights) using signed BITs as a covariate, a number of dispute measures, and typical control variables.'
+	), include.rownames=FALSE,
+	file='modelResultsInvProfileV1.tex'
+	# file='modelResultsPropRightsV1.tex'
+	# file='modelResultsInvProfileV2.tex'
+	# file='modelResultsPropRightsV2.tex'
+	)
 
 # rownames(modelLatex) <- c(
 # 	'Property Rights$_{t-1}$',
@@ -147,9 +169,3 @@ varsTable <- c(
 # 	'Ratified BITs$_{t-1}$',
 # 	'Conc. Dispute$_{t-1}$'
 # 	)
-
-# # Prelim graphics
-# setwd(pathGraphics)
-# pdf(file='ch_inv_hist.pdf')
-# hist(modelData$ch_Investment.Profile)
-# dev.off()
