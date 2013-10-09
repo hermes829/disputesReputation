82ce5d7a62bc3bd6f66b6fdb2b5e2a2bd4288a08
hermes829 <hermes829@gmail.com>
2013-10-09 00:34:29 -0400

    upd models for email to karen

--- RCode/Analysis/ECM_Models.R
+++ RCode/Analysis/ECM_Models.R
@@ -1,5 +1,4 @@
 ### Run ECM analysis
-
 ### Load setup
 source('/Users/janus829/Desktop/Research/RemmerProjects/disputesReputation/RCode/setup.R')
 
@@ -11,51 +10,44 @@ dirt=c('pch_','lag_','distC_','distD_','distD2_')
 for(ii in 1:length(dirt)){allVars=gsub(dirt[ii],'',allVars)}
 unique(allVars)
 
-# Adding democracy binary
-allData$democ=as.numeric(allData$polity>=16)
-
 # dep='Investment.Profile'
 dep='Property.Rights'
 dv=paste('pch_', dep, sep='')
 
-covs <- c(
+covs = c(
 	'cicsidcase', 'signedbits',
 	'LNgdpCAP', 'LNpopulation',
-	'domSUM', 
-	'kaopen',
-	# 'polity',
-	'xconst',
-	'polconiii')
+	'domSUM', 'kaopen'
+	,'X2F.Legal.enforcement.of.contracts'
+	)
 covs=unlist(lapply(covs, function(x) FUN=paste(c('pch_', 'lag_'),x,sep='')))
 
 # Adding other covariates
 # Lagged DV
 covs=c(paste('lag_',dep,sep=''),covs)
-# Developed Country Binary
-covs=c('oecd',covs)
+# Upper Income Binary
+covs=c('upperincome',covs)
 # Democracy Binary
-# covs=c('democ',covs)
+covs=c('democ',covs)
 
-ids=c('ccode','year')
-vars=c(dv, covs, ids)
+# Adding ids & combining list of vars
+ids=c('ccode','year'); vars=c(dv, covs, ids)
 
-dim(allData)
-dim(na.omit(allData[,vars]))
+# Sample size stats
+dim(allData); length(unique(allData$ccode)); length(unique(allData$year))
+dim(na.omit(allData[,vars])); length(unique(na.omit(allData[,vars])[,'ccode'])); length(unique(na.omit(allData[,vars])[,'year']))
 
-mform=paste(
-	paste(dv, paste(covs,collapse=' + '), sep=' ~ ') )
+mform=paste(paste(dv, paste(covs,collapse=' + '), sep=' ~ ') )
 
 # RE Models
-reform=formula(paste(mform,
-			' + (1 | ccode)', sep=''))
-remodel <- lmer(reform, data=allData )
+reform=formula(paste(mform, ' + (1 | ccode)', sep=''))
+remodel = lmer(reform, data=allData )
 attributes(summary(remodel))$coefs
 sqrt(mean((resid(remodel))^2))
 
 # FE Models
-feform=formula(paste(mform,
-		' + as.factor(ccode) - 1', sep=''))
-femodel <- lm(feform, data=allData )
+feform=formula(paste(mform, ' + as.factor(ccode) - 1', sep=''))
+femodel = lm(feform, data=allData )
 summary(femodel)$coefficients[1:(length(covs)+1),]
 sqrt(mean(femodel$residuals^2))
 summary(femodel)$r.squared
