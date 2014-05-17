6e2134d6057c7af7f374be6f3fb5db323536ffc5
hermes829 <hermes829@gmail.com>
2014-05-17 13:44:56 -0700

    diff dist by each dispute var

--- RCode/Analysis/Level_Sims.R
+++ RCode/Analysis/Level_Sims.R
@@ -26,85 +26,92 @@ modelData = modelData[modelData$year>1986,]
 setwd(pathResults)
 load('LinvProfFE.rda'); dv='Investment Profile'
 
-x=modResults[[1]] # model results for vars similar across dispute variables
-coefs=ivAll[[1]]; coefName=ivsName
-estimates=x$coefficient
-varcov=vcovHC(x, method="arellano", cluster="time")
-colnames(varcov)=rownames(varcov)=names(estimates)
-serrors=sqrt(diag( varcov ))
-error = sqrt(sum(x$residuals^2)/x$df.residual)
-##########################################################################################
+diffPlots=list()
+for(ii in 1:length(modResults)){
 
-##########################################################################################
-varsTable=unlist(lapply(ivs, function(x) FUN=paste( c('lag_'), x, sep='' )))
-lagLabName=function(x){ paste(x, '$_{t-1}$', sep='') }
-varsTableNames=unlist( lapply(ivsName, function(x) FUN= c(lagLabName(x))) )
-varDef=cbind(varsTable, varsTableNames)
-varDef=varDef[c(1,6:nrow(varDef)),]
-##########################################################################################
+  x=modResults[[ii]] # model results for vars similar across dispute variables
+  vars=ivAll[[ii]]; coefName=ivsName
+  estimates=x$coefficient
+  varcov=vcovHC(x, method="arellano", cluster="time")
+  colnames(varcov)=rownames(varcov)=names(estimates)
+  serrors=sqrt(diag( varcov ))
+  error = sqrt(sum(x$residuals^2)/x$df.residual)
+  ##########################################################################################
 
-#########################################################################################
-# Distributions
-# Plotting density distributions
-vi=varDef[1,1]
-sims=10000
-simData=na.omit(modelData)
-vars=coefs
-vRange=quantile(modelData[,vi],probs=seq(0,1,.1))[c(2,10)]
-ostat=median
-betas=estimates
-vcov=varcov
-sigma=error
-intercept=FALSE
-specX=FALSE
-specY=TRUE
-ylabel="Inv. Profile$_{t}$"
+  ##########################################################################################
+  varsTable=unlist(lapply(ivs, function(x) FUN=paste( c('lag_'), x, sep='' )))
+  lagLabName=function(x){ paste(x, '$_{t-1}$', sep='') }
+  varsTableNames=unlist( lapply(ivsName, function(x) FUN= c(lagLabName(x))) )
+  varDef=cbind(varsTable, varsTableNames)
+  varDef=varDef[c(ii,6:nrow(varDef)),]
+  ##########################################################################################
 
-# Set up scenario
-scenCol = length(vars); scenRow = length(vRange)
-scenario = matrix(NA, nrow=scenRow, ncol=scenCol)
-colnames(scenario) = c(vars)
-scenario[,vi] = vRange
+  #########################################################################################
+  # Distributions
+  # Plotting density distributions
+  vi=varDef[1,1]
+  sims=10000
+  simData=na.omit(modelData)
+  vRange=quantile(simData[,vi],probs=seq(0,1,.1))[c(2,10)]
+  intercept=FALSE
+  specX=FALSE
+  specY=TRUE
+  ylabel="Inv. Profile$_{t}$"
 
-viPos = which(vi==vars)
-ovals = apply(simData[,vars[-viPos]], 2, ostat)
-scenario[,vars[-viPos]] = matrix(rep(ovals,scenRow),nrow=scenRow,byrow=TRUE)
-if(intercept){scenario = cbind('(Intercept)'=1, scenario)}
-vars2 = colnames(scenario)
+  # Set up scenario
+  scenCol = length(vars); scenRow = length(vRange)
+  scenario = matrix(NA, nrow=scenRow, ncol=scenCol)
+  colnames(scenario) = c(vars)
+  scenario[,vi] = vRange
 
-draws = mvrnorm(n = sims, betas[vars2], vcov[vars2,vars2])
-modelPreds = draws %*% t(scenario)
-modelExp = apply(modelPreds, 2, function(x) FUN=rnorm(sims, x, sigma))
+  viPos = which(vi==vars)
+  ovals = apply(simData[,vars[-viPos]], 2, median)
+  scenario[,vars[-viPos]] = matrix(rep(ovals,scenRow),nrow=scenRow,byrow=TRUE)
+  if(intercept){scenario = cbind('(Intercept)'=1, scenario)}
+  vars2 = colnames(scenario)
 
-# One difference distribution
-diff=data.frame(modelPreds[,1]-modelPreds[,2]); colnames(diff)='value'
-# diff=data.frame(modelExp[,1]-modelExp[,2]); colnames(diff)='value'
-diff$X2=1
+  draws = mvrnorm(n = sims, estimates[vars2], varcov[vars2,vars2])
+  modelPreds = draws %*% t(scenario)
+  modelExp = apply(modelPreds, 2, function(x) FUN=rnorm(sims, x, error))
 
-ggMeans = ddply(diff, .(X2), summarise, sMean=mean(value))
-ggDensity = ddply(diff, .(X2), .fun=function(x){
-  tmp = density(x$value); x1 = tmp$x; y1 = tmp$y
-  q95 = x1 >= quantile(x$value,0.025) & x1 <= quantile(x$value,0.975)
-  q90 = x1 >= quantile(x$value,0.05) & x1 <= quantile(x$value,0.95)
-  data.frame(x=x1,y=y1,q95=q95, q90=q90) } )
+  # One difference distribution
+  diff=data.frame(modelPreds[,1]-modelPreds[,2]); colnames(diff)='value';diff$X2=1
+  # diff=data.frame(modelExp[,1]-modelExp[,2]); colnames(diff)='value';diff$X2=1
 
-ggMeans$X2 = as.factor(ggMeans$X2)
-ggDensity$X2 = as.factor(ggDensity$X2)
+  # Dist for each scenario
+  # diff=melt(modelPreds)[,-1]
 
-temp = ggplot()
-temp = temp + geom_line(data=ggDensity, aes(x=x,y=y),size=1.5)
-temp = temp + geom_vline(data=ggMeans,
-  aes(xintercept=sMean),linetype='solid',size=1)
-temp = temp + geom_ribbon(data=subset(ggDensity,q95),
-  aes(x=x,ymax=y),ymin=0,alpha=0.3)
-temp = temp + geom_ribbon(data=subset(ggDensity,q90),
-  aes(x=x,ymax=y),ymin=0,alpha=0.6)
-temp = temp + xlab(ylabel) + ylab('Density')
-temp = temp + theme(legend.position='none', legend.title=element_blank(),
-  axis.ticks = element_blank(), 
-  panel.grid.major=element_blank(), panel.grid.minor=element_blank(), 
-  axis.title.x = element_text(vjust=-0.2), 
-  axis.title.y = element_text(vjust=0.2),
-  panel.border = element_blank(), axis.line = element_line(color='black'))
-temp
+  ggMeans = ddply(diff, .(X2), summarise, sMean=mean(value))
+  ggDensity = ddply(diff, .(X2), .fun=function(x){
+    tmp = density(x$value); x1 = tmp$x; y1 = tmp$y
+    q95 = x1 >= quantile(x$value,0.025) & x1 <= quantile(x$value,0.975)
+    q90 = x1 >= quantile(x$value,0.05) & x1 <= quantile(x$value,0.95)
+    data.frame(x=x1,y=y1,q95=q95, q90=q90) } )
+
+  ggMeans$X2 = as.factor(ggMeans$X2)
+  ggDensity$X2 = as.factor(ggDensity$X2)
+
+  temp = ggplot()
+  temp = temp + geom_line(data=ggDensity, aes(x=x,y=y),size=1.5)
+  # temp = temp + geom_line(data=ggDensity, aes(x=x,y=y,fill=X2,color=X2,linetype=X2),size=1.5)
+  temp = temp + geom_vline(data=ggMeans,
+    aes(xintercept=sMean),linetype='solid',size=1)
+  temp = temp + geom_ribbon(data=subset(ggDensity,q95),
+    aes(x=x,ymax=y),ymin=0,alpha=0.3)
+    # aes(x=x,ymax=y,fill=X2),ymin=0,alpha=0.3)
+  temp = temp + geom_ribbon(data=subset(ggDensity,q90),
+    aes(x=x,ymax=y),ymin=0,alpha=0.6)
+    # aes(x=x,ymax=y,fill=X2),ymin=0,alpha=0.6)
+  temp = temp + xlab(ylabel) + ylab('Density')
+  temp = temp + theme(legend.position='none', legend.title=element_blank(),
+    axis.ticks = element_blank(), 
+    panel.grid.major=element_blank(), panel.grid.minor=element_blank(), 
+    axis.title.x = element_text(vjust=-0.2), 
+    axis.title.y = element_text(vjust=0.2),
+    panel.border = element_blank(), axis.line = element_line(color='black'))
+  # temp
+  diffPlots[[ii]]=temp
+}
+
+multiplot(diffPlots, 2)
 #########################################################################################
