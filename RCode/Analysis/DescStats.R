3774adb2a73bfb54d2e7c5c8eb5d19b5cebd97f4
hermes829 <hermes829@gmail.com>
2014-05-20 22:29:42 -0700

    added desc stats

---
+++ RCode/Analysis/DescStats.R
@@ -0,0 +1,87 @@
+### Load setup
+source('/Users/janus829/Desktop/Research/RemmerProjects/disputesReputation/RCode/setup.R')
+
+# Directly loading in Karen's data
+setwd(paste(pathData, '/Components', sep=''))
+modelData=read.dta('Investment Profile Data.7.dta')
+colnames(modelData)[colnames(modelData)=='lagcumcunctadcase']='lag_cumcunctadcase'
+colnames(modelData)[colnames(modelData)=='lagcum_icsidtreaty_case']='lag_cum_icsidtreaty_case'
+colnames(modelData)[colnames(modelData)=='lagcum_kicsidcase']='lag_cum_kicsidcase'
+colnames(modelData)[colnames(modelData)=='lagpch_gdp']='lag_pch_gdp'
+
+lagVars=c('cumunsettled_icsid_treaty','cum_alltreaty')
+modelData$cyear=numSM(modelData$cyear)
+modelData=lagDataSM(modelData, 'cyear', 'ccode', lagVars, 1)
+colnames(modelData)[(ncol(modelData)-1):ncol(modelData)]=paste0('lag_',lagVars)
+
+modelData = modelData[modelData$upperincome==0,]
+modelData = modelData[modelData$year>1986,]
+##########################################################################################
+
+##########################################################################################
+# Subsetting to vars of interest
+dv='Investment_Profile'
+
+ivDisp=c('cum_kicsidcase','cum_icsidtreaty_case',
+	'cumunsettled_icsid_treaty','cumcunctadcase','cum_alltreaty' )
+
+# Other covariates
+ivOther=c(
+	'pch_gdp'
+	,'LNpopulation'
+	,'lncinflation'
+	, 'Internal_Conflict'	
+	,'ratifiedbits'	
+	,'kaopen'	
+	,'polity'
+	)
+
+vars=c(dv, ivDisp, ivOther)
+
+# Setting up variables names for display
+dvName='Investment Profile'
+ivDispName=c('All ICSID Disputes', 'ICSID Treaty-Based', 'Unsettled ICSID', 'UNCTAD','ICSID-UNCTAD' )
+ivOtherName=c(
+	'\\%$\\Delta$ GDP'
+	, 'Ln(Pop.)'
+	, 'Ln(Inflation)'	
+	, 'Internal Stability'	
+	,'Ratif. BITs'	
+	,'Capital Openness'	
+	,'Polity'
+	)
+varNames=c(dvName, ivDispName, ivOtherName)
+
+# Subsetting dataset
+anData=modelData[,c(vars,'year','ccode')]
+##########################################################################################
+
+##########################################################################################
+# Running desc stats
+summSM=function(var,group,data){
+	slice=na.omit(data[,c(var,group)])
+	N=length(unique(slice[,group])); n=nrow(slice)
+	avg=mean(slice[,var]); std=sd(slice[,var])
+	lo=min(slice[,var]); hi=max(slice[,var])
+	c(N, n, avg, std, lo, hi)
+}
+
+results=matrix(unlist(lapply(vars, function(x) FUN=summSM(x, 'ccode', anData))),
+	ncol=6,byrow=TRUE,
+	dimnames=c(list(varNames, c('N','n','Mean','Std. Dev.', 'Min','Max')))
+	)
+round(results,2)
+
+setwd(pathPaper)
+print.xtable(xtable(round(results,2), align='llccccc', 
+	digits=c(0,0,0,2,2,2,2),
+	# caption=captionTable
+	), include.rownames=TRUE,
+	# sanitize.text.function = function(x) x,
+	# sanitize.text.function=function(str)gsub("_","\\_",str,fixed=TRUE),
+	sanitize.text.function = identity,		
+	hline.after=c(0,0,nrow(results),nrow(results)), 	
+	size="footnotesize",	
+	file='descTable.tex'
+	)
+##########################################################################################
