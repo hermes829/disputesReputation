adde8aa488551add30100220cb8dcd702e772a71
hermes829 <hermes829@gmail.com>
2015-07-09 19:29:01 -0700

    process disputes data

---
+++ RCode/Data/getData/getDisputes.R
@@ -0,0 +1,7 @@
+if(Sys.info()["user"]=="janus829" | Sys.info()["user"]=="s7m"){
+	source('~/Research/RemmerProjects/disputesReputation/RCode/setup.R') }
+
+###############################################################
+# Load in data
+
+###############################################################	
