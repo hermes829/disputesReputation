c0f5a063b3587f1c333815912060c64db98d1f5a
hermes829 <hermes829@hotmail.com>
2016-02-24 17:26:14 -0800

    minor edits

--- RCode/Analysis/Level/Level_FDI_Models.R
+++ RCode/Analysis/Level/Level_FDI_Models.R
@@ -7,7 +7,7 @@ if(Sys.info()["user"]=="janus829" | Sys.info()["user"]=="s7m"){
 load(paste0(pathBin, 'analysisData.rda'))
 #######################################################################################
 # Setting up models
-dv='invProf'; dvName='Ln(FDI)'; fileFE='fdiFE.rda'
+dv='rfdiLog'; dvName='Ln(FDI)'; fileFE='fdiFE.rda'
 
 # disputes
 dispVars =  c( 'iDispB')
@@ -96,7 +96,7 @@ tmp = tmp + theme(
 	)
 tmp
 setwd(pathGraphics)
-tikz(file='corrFDI.tex',width=15,height=3.5,standAlone=F)
+tikz(file='corrFDI.tex',width=15,height=4.5,standAlone=F)
 tmp
 dev.off()
 #######################################################################################
